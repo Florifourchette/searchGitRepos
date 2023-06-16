d8a4f6646525c6646c7d9a789a1f76d12c1c7a3f
Florifourchette <florifourchette@gmail.com>
2023-06-16 16:37:35 +0200

    owner card has been done

--- src/components/GitResults.jsx
+++ src/components/GitResults.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { getGitData } from '../utils/RepoAPICall';
-import { List, Image, Card } from 'semantic-ui-react';
-import '../styles/stylesheet.css';
+import { List, Image, Card, CardContent } from 'semantic-ui-react';
 
-const GitResults = ({ searchTerm }) => {
+const GitResults = ({ searchTerm, handleClick }) => {
   const [data, setData] = useState([]);
 
   console.log(searchTerm);
 
   useEffect(() => {
     console.log(searchTerm);
-    getGitData(searchTerm).then((data) => setData(data.items));
+    getGitData(searchTerm)
+      .then((data) => setData(data.items))
+      .catch((error) => console.log(error));
   }, [searchTerm]);
 
   console.log(data);
 
   return (
-    <>
+    <div classname="Search_Result_Container">
       <List>
         {data?.map((item) => (
-          <List.Item className="result_card" key={data.indexOf(item)}>
+          <List.Item
+            className="result_card"
+            key={data.indexOf(item)}
+            onClick={() => handleClick(item.owner.login)}
+          >
             <Image
               className="avatar"
               avatar
@@ -32,7 +37,7 @@ const GitResults = ({ searchTerm }) => {
           </List.Item>
         ))}
       </List>
-    </>
+    </div>
   );
 };
 
